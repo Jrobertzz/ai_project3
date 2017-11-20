03e2418841e07a5f6a7981b0723e1fb31352cc27
Jacob Wilson <wilsonjj2@vcu.edu>
2017-11-20 10:29:31 -0500

    added results display
    results display with slight modification to testing function to include all errors not just total

--- proj_3.m
+++ proj_3.m
@@ -8,7 +8,7 @@ train_data = [train_data_1;train_data_2;train_data_3];
 Alpha = .00001;
 
 [weight,error1] = deltaTraining(train_data,Alpha);
-error2 = testing(test_data_4,weight);
+[error2,errors2] = testing(test_data_4,weight);
 
 figure
 plot (test_data_4(1:9,1),test_data_4(1:9,2),'+r')
@@ -21,7 +21,58 @@ title(title1)
 xlabel('time (hr)')
 ylabel('temp (F)')
 
-function error = testing(testingData,perceptron)
+disp("Learning Rate:")
+disp(Alpha)
+disp("Iterations:")
+ite = 1000000;
+disp(ite)
+
+labels = ["Hour","Pred","Real","Error"];
+
+disp("Day 1: Training Error")
+disp(labels)
+[errorD1,errorsD1] = testing(train_data_1,weight);
+for i = 1:9
+    temp = -1*(weight(1,3)+weight(1,1)*(i+4))/weight(1,2);
+    output = [i+4,temp,train_data_1(i,2),errorsD1(i,1)];
+    disp(output)
+end
+disp("Total Error:")
+disp(errorD1(1,1))
+
+disp("Day 2: Training Error")
+disp(labels)
+[errorD2,errorsD2] = testing(train_data_2,weight);
+for i = 1:9
+    temp = -1*(weight(1,3)+weight(1,1)*(i+4))/weight(1,2);
+    output = [i+4,temp,train_data_2(i,2),errorsD2(i,1)];
+    disp(output)
+end
+disp("Total Error:")
+disp(errorD2(1,1))
+   
+disp("Day 3: Training Error")
+disp(labels)
+[errorD3,errorsD3] = testing(train_data_3,weight);
+for i = 1:9
+    temp = -1*(weight(1,3)+weight(1,1)*(i+4))/weight(1,2);
+    output = [i+4,temp,train_data_3(i,2),errorsD3(i,1)];
+    disp(output)
+end
+disp("Total Error:")
+disp(errorD3(1,1))
+
+disp("Day 4: Testing Error")
+disp(labels)
+for i = 1:9
+    temp = -1*(weight(1,3)+weight(1,1)*(i+4))/weight(1,2);
+    output = [i+4,temp,test_data_4(i,2),errors2(i,1)];
+    disp(output)
+end
+disp("Total Error:")
+disp(error2(1,1))
+
+function [error,errors] = testing(testingData,perceptron)
     np = size(testingData,1);
     weights = perceptron;
     desired = zeros(np,1);
@@ -31,8 +82,8 @@ function error = testing(testingData,perceptron)
         
     end
     outputTotal = testingData(:,1)*weights(1,1)+testingData(:,2)*weights(1,2)+weights(1,3);
-
     error = sum((desired(:,1)-outputTotal(:,1)).^2);
+    errors = desired(:,1)-outputTotal(:,1);
 end
 
 function [weights,error] = deltaTraining(trainingData,learningConst)
